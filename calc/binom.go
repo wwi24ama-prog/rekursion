a366a7a0955cb111c36695dd7ed8bb36984b183c
Reiner Huechting <dev@reinerhuechting.de>
2024-12-06 13:26:36 +0100

    Code aus Vorlesung hinzugefügt.

--- calc/binom.go
+++ calc/binom.go
@@ -3,6 +3,19 @@ package calc
 // BinomialCoefficient erwartet zwei Zahlen n und k und liefert den
 // Binomialkoeffizienten "n über k".
 func BinomialCoefficient(n, k int) int {
-	// TODO
-	return 0
+	if k == 0 || k == n {
+		return 1
+	}
+
+	return BinomialCoefficient(n-1, k-1) + BinomialCoefficient(n-1, k)
+
+	//return factorial(n) / (factorial(k) * factorial(n-k))
 }
+
+// func factorial(n int) int {
+// 	if n == 0 {
+// 		return 1
+// 	}
+
+// 	return n * factorial(n-1)
+// }
--- lists/filter.go
+++ lists/filter.go
@@ -4,8 +4,22 @@ package lists
 // Verwenden Sie Rekursion und benutzen Sie NICHT die len-Funktion.
 // Sie können die Hilfsfunktion Empty aus empty.go verwenden.
 func FilterLess(list []int, key int) []int {
-	// TODO
-	return []int{}
+	lesslist := []int{}
+
+	FilterLessHelper(list, key, &lesslist)
+
+	return lesslist
+}
+
+func FilterLessHelper(list []int, key int, lesslist *[]int) {
+	if Empty(list) {
+		return
+	}
+	if list[0] <= key {
+		*lesslist = append(*lesslist, list[0])
+	}
+
+	FilterLessHelper(list[1:], key, lesslist)
 }
 
 // Liefert eine Liste mit allen Elementen aus list, die echt größer als key sind.
